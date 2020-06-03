50af6f2d29d2abbff7bbf971dba6c0ccc57dd8c1
Daniel Rodriguez <danca9409@gmail.com>
2020-06-03 15:16:58 -0500

    ultimo

--- src/control/Quotation_Controller.java
+++ src/control/Quotation_Controller.java
@@ -15,25 +15,26 @@ import utils.paneColor;
  * @author Daniel
  */
 public class Quotation_Controller {
+
     private final Quotation q = new Quotation();
     private final paneColor pc = new paneColor();
-    
-    public void registerQuotation(int no, Customer c, int t, int p){
+
+    public void registerQuotation(int no, Customer c, int t, int p) {
         q.setC(c);
         q.setNo(no);
         q.setT(t);
         q.setP(p);
         pc.getPanel(null, "Quotation Registred!", "Registred", JOptionPane.INFORMATION_MESSAGE);
     }
-    
-    public Object loadQuotation(){
+
+    public Object loadQuotation() {
         Object[] o = new Object[4];
-        o[0] = q.getNo();
-        o[1] = q.getC();
-        o[2] = q.getP();
-        o[3] = q.getT();
-        
+        o[0] =q.getNo();
+        o[1] =q.getC();
+        o[2] =q.getP();
+        o[3] =q.getT();
+
         return o;
     }
-    
+
 }
